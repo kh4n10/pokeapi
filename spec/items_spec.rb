4060a3ba9b8f55e88170be9f9c4c36b5ff967a63
Ginxty <james.oneill96@gmail.com>
2017-10-30 12:22:26 +0000

    added 200 for items machines and moves

--- spec/items_spec.rb
+++ spec/items_spec.rb
@@ -1,3 +1,18 @@
 require "httparty"
 require "pokeapi"
 require "rspec"
+
+
+describe Pokeapi do
+  context "given a request" do
+    it "responds" do
+      expect(Pokeapi.get_the("item",1).code).to eql(200)
+      expect(Pokeapi.get_the("item-attribute",1).code).to eql(200)
+      expect(Pokeapi.get_the("item-category",1).code).to eql(200)
+      expect(Pokeapi.get_the("item-fling-effect",1).code).to eql(200)
+      expect(Pokeapi.get_the("item-pocket",1).code).to eql(200)
+    end
+  end
+
+
+end
