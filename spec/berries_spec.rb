cde39f2057e1f74ef0245e5755099e4504020036
Luke Hemingway <lukehemingway66@gmail.com>
2017-10-30 14:42:24 +0000

    Just wanting to pull down like

--- spec/berries_spec.rb
+++ spec/berries_spec.rb
@@ -19,18 +19,16 @@ describe Pokeapi do
   end
 
   describe "When given a resouce and no ID" do
-      context "Return the resouce list" do
-        it "Returns code  and gives the ok message(200)" do
-          expect(Pokeapi.get_the_resource("berry").code).to eql(200)
-        end
-
-        it "Returns the resouce list as a Hash" do
-          expect(Pokeapi.get_the_resource("berry").parsed_response).to be_instance_of Hash
-        end
-
+    context "Return the resouce list" do
+      it "Returns code  and gives the ok message(200)" do
+        expect(Pokeapi.get_the_resource("berry").code).to eql(200)
       end
 
+      it "Returns the resouce list as a Hash" do
+        expect(Pokeapi.get_the_resource("berry").parsed_response).to be_instance_of Hash
+      end
+      
     end
-
-
+  end
+end
 end
