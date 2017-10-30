187de81a16d7f2b866a1703e223665d258a61611
= <=>
2017-10-30 12:19:19 +0000

    Adding the tests for all the location endpoints, checking they have the okay code and it has been returned as a JSON file

--- spec/locations_spec.rb
+++ spec/locations_spec.rb
@@ -1,3 +1,50 @@
 require "httparty"
 require "pokeapi"
 require "rspec"
+
+describe "Pokeapi" do
+
+	describe "When the locations resource has been selected" do
+		context "A location id has been inputted" do
+
+			it "Returns the code and gives the code to say its okay" do
+				expect(Pokeapi.get_the("location", 1).code).to eql(200)
+			end
+
+			it "Returns the data as a JSON file" do
+				expect(Pokeapi.get_the("location", 1).parsed_response).to be_instance_of Hash
+			end
+		end
+
+		context "A location-area id has been inputted" do
+			it "Returns the code and gives the code to say its okay" do
+				expect(Pokeapi.get_the("location-area", 1).code).to eql(200)
+			end
+
+			it "Returns the data as a JSON file" do
+				expect(Pokeapi.get_the("location-area", 1).parsed_response).to be_instance_of Hash
+			end
+		end
+
+		context "A pal-park-area id has been inputted" do
+			it "Returns the code and gives the code to say its okay" do
+				expect(Pokeapi.get_the("pal-park-area", 1).code).to eql(200)
+			end
+
+			it "Returns the data as a JSON file" do
+				expect(Pokeapi.get_the("pal-park-area", 1).parsed_response).to be_instance_of Hash
+			end
+		end
+
+		context "A region id has been inputted" do
+			it "Returns the code and gives the code to say its okay" do
+				expect(Pokeapi.get_the("region", 1).code).to eql(200)
+			end
+
+			it "Returns the data as a JSON file" do
+				expect(Pokeapi.get_the("region", 1).parsed_response).to be_instance_of Hash
+			end
+		end
+	end
+
+end
