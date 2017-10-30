41b4eab8b207f928c4d0b16827e295456d7e0c2c
Liam North <liam_north@outlook.com>
2017-10-30 12:23:14 +0000

    Added more tests to check response code 200 for all sub-resources for berries and contests

--- spec/berries_spec.rb
+++ spec/berries_spec.rb
@@ -5,12 +5,30 @@ require "rspec"
 describe Pokeapi do
   before(:all) do
     @a_berry = Pokeapi.get_the("berry", 1)
+    @a_firmness = Pokeapi.get_the("berry-firmness", 1)
+    @a_flavor = Pokeapi.get_the("berry-flavor", 1)
   end
 
   describe "In the berries resource" do
     context "when requesting a berry" do
-      it "returns code 200 to say everything is okay" do
+      it "returns response code 200 to say everything is okay" do
         expect(@a_berry.code).to eql(200)
+      end
+    end
+  end
+
+  describe "In the berry-firmness resource" do
+    context "when requesting a firmness" do
+      it "returns response code 200 to say everything is okay" do
+        expect(@a_firmness.code).to eql(200)
+      end
+    end
+  end
+
+  describe "In the berry-flavor resource" do
+    context "when requesting a flavor" do
+      it "returns response code 200 to say everything is okay" do
+        expect(@a_flavor.code).to eql(200)
       end
     end
   end
